1bfca2b74d7978240b8868c1e42d8602ac670282
HoseaNganga <hoseanganga27@gmail.com>
2024-01-24 17:09:50 +0300

    refactored javascript code

--- script.js
+++ script.js
@@ -1,56 +1,96 @@
-//GET THE RELEVANT DOM ELEMENTS
-const submitForm=document.getElementById(`submitTodo`);
-const inputForm=document.getElementById(`submitButton`);
-const ulEl=document.getElementById(`todoList`);
+const Form = document.querySelector(`.addTodoForm`);
+const listContainer = document.querySelector(`.listContainer`);
+const input = document.getElementById(`todo`);
+//FUNCTION TO FIND HIGHEST ID
+function findHighestId() {
+  const existingCheckboxes = document.querySelectorAll(".checkbox-round");
+  let id = 0;
 
-
-const storedValues=JSON.parse(localStorage.getItem(`todos`))||[];
-
-storedValues.forEach((value)=>{
-    logTodo(value);
-})
-
-//ADD EVENT LISTENER TO FORM ELEMENT TO LISTEN TO SUBMIT EVENT
-submitForm.addEventListener(`submit`,(e)=>{
-    e.preventDefault();
-    logTodo();
-})
-
-function logTodo(todo){
-    //GET INPUT VALUE
-    let inputValue=inputForm.value;
-    //CREATE A LIST ELEMENT TO APPEND THE VALUE TO THE CURRENT LIST
-    if(todo){
-        inputValue=todo.todo;
+  existingCheckboxes.forEach((checkbox) => {
+    const idNumber = parseInt(checkbox.id.replace("checkbox-", ""), 10);
+    if (!isNaN(idNumber) && idNumber > id) {
+      id = idNumber;
     }
-    const newLiEl=document.createElement(`li`);
-    newLiEl.className=`todoitem`;
-    if(todo && todo.completed){
-        newLiEl.classList.add(`completed`);
-    }
-    newLiEl.appendChild(document.createTextNode(inputValue));
-    ulEl.appendChild(newLiEl);
+  });
 
-    inputForm.value=``;
-
-    //FUNCTION TO DELETE ITEM...ADD EVENT LISTENER TO THE ULELEMENT
-    newLiEl.addEventListener(`click`,()=>{
-        newLiEl.classList.toggle(`completed`);
-        updateLs();
-    })
-    updateLs();
+  return id;
 }
 
-function updateLs(){
-    const todoValue=document.querySelectorAll(`.todoitem`);
-    const arrayToStoreValues=[];
-    todoValue.forEach((val)=>{
-        arrayToStoreValues.push(
-            {
-            todo:val.innerText,
-            completed:val.classList.contains(`completed`)
-            })
-    })
-    localStorage.setItem(`todos`,JSON.stringify(arrayToStoreValues));
+let id = findHighestId();
+
+const data = JSON.parse(localStorage.getItem(`todoData`) || "[]");
+Array.from(data).forEach((val) => {
+  loadItems(val);
+});
+Form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const getVal = input.value;
+  const newItem = {
+    id: findHighestId() + 1,
+    text: getVal,
+    checked: false,
+  };
+  loadItems(newItem);
+  input.value = "";
+});
+
+function loadItems(item) {
+  const liEl = document.createElement(`li`);
+  liEl.className = "listItem";
+  liEl.innerHTML = `<div class="flex">
+  <input type="checkbox" id="${item.id}" class="checkbox-round" ${
+    item.checked ? "checked" : ""
+  } />
+
+  <label for="${item.id}">${item.text}</label>
+</div>
+<button
+  class="deleteBtn"
+  onMouseOver="this.style.backgroundColor='red'"
+  onMouseOut="this.style.backgroundColor='green'"
+>
+  <i class="fa-solid fa-trash-can"></i>
+</button>`;
+  listContainer.appendChild(liEl);
+  input.value = "";
+
+  const deleteBtn = liEl.querySelector(".deleteBtn");
+  deleteBtn.addEventListener(`click`, (e) => {
+    const childEl = e.target.parentElement.parentElement;
+    childEl.remove();
+    updateLocalStorage();
+  });
+
+  const inputs = liEl.querySelectorAll(`.checkbox-round`);
+  Array.from(inputs).forEach((val) => {
+    val.addEventListener(`click`, () => {
+      const parent = val.parentElement.parentElement;
+      if (val.checked) {
+        parent.classList.add(`strike`);
+      } else {
+        parent.classList.remove("strike");
+      }
+      updateLocalStorage();
+    });
+  });
+  updateLocalStorage();
 }
 
+function updateLocalStorage() {
+  const listItems = document.querySelectorAll(".listItem");
+  const todoData = [];
+
+  listItems.forEach((item) => {
+    const checkbox = item.querySelector(".checkbox-round");
+    const label = item.querySelector("label");
+
+    todoData.push({
+      id: checkbox.id,
+      text: label.textContent,
+      checked: checkbox.checked,
+    });
+  });
+
+  // Save todoData to local storage
+  localStorage.setItem("todoData", JSON.stringify(todoData));
+}
